59fdf2bdd3e1970f7be5624840b1048281d6511d
LukeDuffin <sectione3d@gmail.com>
2013-07-08 15:30:50 +0100

    Added base file

---
+++ towerdefence/towerdefence.py
@@ -0,0 +1,44 @@
+import pygame
+import random
+
+def grid(pixel):
+	x = pixel * 10
+	return x
+
+class Tower:
+	"""Tower"""
+
+	def __init__(self, surface):
+		a
+
+	def event():
+		a
+
+	def rotate():
+		a
+
+	def click():
+		a
+
+	def 
+running = 1
+width = 640
+height = 400
+
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+
+linecolor = 255, 255, 255
+bgcolor = 0, 0, 0
+
+while running:
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = 0
+		elif event.type == pygame.MOUSEDOWN:
+
+	screen.fill(bgcolor)
+
+	pygame.display.flip()
+	clock.tick(240)
